42a933b8b1dc397c4bf4a9589a8272b3b054770a
shaker <hydrashaker@gmail.com>
2024-11-07 18:49:51 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-  
-  
-  const parent = React.createElement("div", {id:parent },
-    React.createElement("div", {id: "child"},[
-        React.createElement(h1, {}, "i'm h1"),
-        React.createElement("h1", {}, "i'm h2")
-        
-    ])
-  )
-  
-  console.log(parent);
-  
-  
-  //const heading = React.createElement("h1", {id: "heading"}, "Hey From React")
 
-   const root = ReactDOM.createRoot(document.getElementById("root"))
+// const heading = React.createElement("h1", {id: "heading"}, "Shaker.dev");
 
-   root.render(parent)
+// const jsxHeading = <h1 className="head">Shaker.dev</h1>
+
+// console.log(jsxHeading);
+
+// console.log(heading);
+
+// const root = ReactDOM.createRoot(document.getElementById(root));
+
+
+
+ //React Components
+ //class based component
+ //functional component 
+
+
+
+//  function functionalComponent(){
+//   return (
+//     <h1>functional component</h1>
+//   )
+//  }
+
+const title = (
+  <p>hello world</p>
+)
+
+const Title = ()=>(
+ <h1 className="head" tabIndex="5">
+  React using jsx</h1>
+)
+
+//component composition 
+const HeadingComponent = ()=> (
+  <div className="container">
+    <h2>{100 + 200}</h2> 
+    {title}
+    <Title />
+    <h1>React Functional Components</h1>
+  </div>
+)
+
+const OuterComponent = ()=> {
+  return
+    <>
+    
+    <div>
+      <h2>paragraph</h2>
+    </div>
+    
+    </>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<HeadingComponent />)
